sonyxperiadev/kernel

driver: staging: Android Low Memory Killer: consider swap free size
Use freeswap as otherfile when watermark is grater than
low_watermark. This helps to make use of zram swap.

Extracted from 39.2.A.0.442

Signed-off-by: Humberto Borba <humberos@gmail.com>
Change-Id: Ic25e10f23dfcc0073bdd7f60d939387499309d82

drivers/staging/android/lowmemorykiller.c
@@ -380,7 +380,59 @@ void tune_lmk_param(int *other_free, int *other_file, struct shrink_control *sc)
 		lowmem_print(4, "lowmem_shrink tunning for others ofree %d, "
 			     "%d\n", *other_free, *other_file);
 	}
+#ifdef CONFIG_ANDROID_LOW_MEMORY_KILLER_CONSIDER_SWAP
+	if (zone_watermark_ok(preferred_zone, 0,
+			  low_wmark_pages(preferred_zone), 0, 0)) {
+		struct sysinfo si;
+		si_swapinfo(&si);
+		*other_free += si.freeswap;
+#ifdef CONFIG_ZRAM
+		/* If swap is actually residing in RAM (e. g. the swap device
+		 * is a ZRAM device), we need to subtract the amount of RAM
+		 * that will be occupied by compressed data. To play on the
+		 * safe side, it's better to subtract too much than too few,
+		 * otherwise LMK may not be triggered when it has to be. ZRAM
+		 * compression ratio is at least 2, so we subtract half of the
+		 * reported freeswap.
+		 */
+		*other_free -= si.freeswap >> 1;
+#endif
+		lowmem_print(4, "lowmem_shrink tunning for swap "
+			     "ofree %d, %d\n", *other_free, *other_file);
+	}
+#endif
 }
+
+#ifdef CONFIG_ANDROID_LOW_MEMORY_KILLER_CONSIDER_SWAP
+static void lowmem_wakeup_kswapds(struct shrink_control *sc, int minfree)
+{
+	gfp_t gfp_mask;
+	struct zone *preferred_zone;
+	struct zonelist *zonelist;
+	enum zone_type high_zoneidx, classzone_idx;
+	int order = 0;
+
+	if (likely(current_is_kswapd()))
+		return;
+
+	gfp_mask = sc->gfp_mask;
+	adjust_gfp_mask(&gfp_mask);
+
+	zonelist = node_zonelist(0, gfp_mask);
+	high_zoneidx = gfp_zone(gfp_mask);
+	first_zones_zonelist(zonelist, high_zoneidx, NULL, &preferred_zone);
+	classzone_idx = zone_idx(preferred_zone);
+
+	for (minfree >>= 13; order < 7; order++) {
+		if (minfree <= (1 << order))
+			break;
+	}
+
+	lowmem_print(4, "lowmem_wakeup_kswapds order %d\n", order);
+	wake_all_kswapds(order, zonelist, high_zoneidx,
+				preferred_zone, NULL);
+}
+#endif
 
 static unsigned long lowmem_scan(struct shrinker *s, struct shrink_control *sc)
 {
@@ -392,6 +444,9 @@ static unsigned long lowmem_scan(struct shrinker *s, struct shrink_control *sc)
 	int ret = 0;
 	short min_score_adj = OOM_SCORE_ADJ_MAX + 1;
 	int minfree = 0;
+#ifdef CONFIG_ANDROID_LOW_MEMORY_KILLER_CONSIDER_SWAP
+	int  max_minfree = 0;
+#endif
 	int selected_tasksize = 0;
 	short selected_oom_score_adj;
 	int array_size = ARRAY_SIZE(lowmem_adj);
@@ -419,6 +474,10 @@ static unsigned long lowmem_scan(struct shrinker *s, struct shrink_control *sc)
 		array_size = lowmem_minfree_size;
 	for (i = 0; i < array_size; i++) {
 		minfree = lowmem_minfree[i];
+#ifdef CONFIG_ANDROID_LOW_MEMORY_KILLER_CONSIDER_SWAP
+		if (max_minfree < minfree)
+			max_minfree = minfree;
+#endif
 		if (other_free < minfree && other_file < minfree) {
 			min_score_adj = lowmem_adj[i];
 			break;
@@ -436,6 +495,9 @@ static unsigned long lowmem_scan(struct shrinker *s, struct shrink_control *sc)
 		lowmem_print(5, "lowmem_scan %lu, %x, return 0\n",
 			     sc->nr_to_scan, sc->gfp_mask);
 		mutex_unlock(&scan_mutex);
+#ifdef CONFIG_ANDROID_LOW_MEMORY_KILLER_CONSIDER_SWAP
+		lowmem_wakeup_kswapds(sc, max_minfree);
+#endif
 		return 0;
 	}
 

Coptan99/android_kernel_xiaomi_sdm660-4.4

Signed-off-by: GhostMaster69-dev <rathore6375@gmail.com>
Signed-off-by: Omar Mohamed <omarcoptan9@gmail.com>

drivers/staging/android/lowmemorykiller.c
@@ -543,7 +543,59 @@ void tune_lmk_param(int *other_free, int *other_file, struct shrink_control *sc)
 		lowmem_print(4, "lowmem_shrink tunning for others ofree %d, "
 			     "%d\n", *other_free, *other_file);
 	}
+#ifdef CONFIG_ANDROID_LOW_MEMORY_KILLER_CONSIDER_SWAP
+	if (zone_watermark_ok(preferred_zone, 0,
+			  low_wmark_pages(preferred_zone), 0, 0)) {
+		struct sysinfo si;
+		si_swapinfo(&si);
+		*other_free += si.freeswap;
+#ifdef CONFIG_ZRAM
+		/* If swap is actually residing in RAM (e. g. the swap device
+		 * is a ZRAM device), we need to subtract the amount of RAM
+		 * that will be occupied by compressed data. To play on the
+		 * safe side, it's better to subtract too much than too few,
+		 * otherwise LMK may not be triggered when it has to be. ZRAM
+		 * compression ratio is at least 2, so we subtract half of the
+		 * reported freeswap.
+		 */
+		*other_free -= si.freeswap >> 1;
+#endif
+		lowmem_print(4, "lowmem_shrink tunning for swap "
+			     "ofree %d, %d\n", *other_free, *other_file);
+	}
+#endif
 }
+
+#ifdef CONFIG_ANDROID_LOW_MEMORY_KILLER_CONSIDER_SWAP
+static void lowmem_wakeup_kswapds(struct shrink_control *sc, int minfree)
+{
+	gfp_t gfp_mask;
+	struct zone *preferred_zone;
+	struct zonelist *zonelist;
+	enum zone_type high_zoneidx, classzone_idx;
+	int order = 0;
+
+	if (likely(current_is_kswapd()))
+		return;
+
+	gfp_mask = sc->gfp_mask;
+	adjust_gfp_mask(&gfp_mask);
+
+	zonelist = node_zonelist(0, gfp_mask);
+	high_zoneidx = gfp_zone(gfp_mask);
+	first_zones_zonelist(zonelist, high_zoneidx, NULL, &preferred_zone);
+	classzone_idx = zone_idx(preferred_zone);
+
+	for (minfree >>= 13; order < 7; order++) {
+		if (minfree <= (1 << order))
+			break;
+	}
+
+	lowmem_print(4, "lowmem_wakeup_kswapds order %d\n", order);
+	_wake_all_kswapds(order, zonelist, high_zoneidx,
+				preferred_zone);
+}
+#endif
 
 static unsigned long lowmem_scan(struct shrinker *s, struct shrink_control *sc)
 {
@@ -555,6 +607,9 @@ static unsigned long lowmem_scan(struct shrinker *s, struct shrink_control *sc)
 	int ret = 0;
 	short min_score_adj = OOM_SCORE_ADJ_MAX + 1;
 	int minfree = 0;
+#ifdef CONFIG_ANDROID_LOW_MEMORY_KILLER_CONSIDER_SWAP
+	int  max_minfree = 0;
+#endif
 	int selected_tasksize = 0;
 	short selected_oom_score_adj;
 	int array_size = ARRAY_SIZE(lowmem_adj);
@@ -583,6 +638,10 @@ static unsigned long lowmem_scan(struct shrinker *s, struct shrink_control *sc)
 		array_size = lowmem_minfree_size;
 	for (i = 0; i < array_size; i++) {
 		minfree = lowmem_minfree[i];
+#ifdef CONFIG_ANDROID_LOW_MEMORY_KILLER_CONSIDER_SWAP
+		if (max_minfree < minfree)
+			max_minfree = minfree;
+#endif
 		if (other_free < minfree && other_file < minfree) {
 			min_score_adj = lowmem_adj[i];
 			break;
@@ -600,6 +659,9 @@ static unsigned long lowmem_scan(struct shrinker *s, struct shrink_control *sc)
 		lowmem_print(5, "lowmem_scan %lu, %x, return 0\n",
 			     sc->nr_to_scan, sc->gfp_mask);
 		mutex_unlock(&scan_mutex);
+#ifdef CONFIG_ANDROID_LOW_MEMORY_KILLER_CONSIDER_SWAP
+		lowmem_wakeup_kswapds(sc, max_minfree);
+#endif
 		return 0;
 	}
 